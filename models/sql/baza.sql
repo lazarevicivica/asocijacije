19977aebff623e21482027134664279f4b41ce12
Strahinja Mihajlovic <strale58@hotmail.com>
2018-10-28 20:37:03 +0100

    Dovrsen filter reci

--- models/sql/baza.sql
+++ models/sql/baza.sql
@@ -16,7 +16,6 @@ CREATE TABLE korisnik (
 
 -- Imutable
 -- filtrirati neprimerene reci?
-<<<<<<< HEAD
 CREATE TABLE pojam(
     id serial PRIMARY KEY,
     kreator_id INT NOT NULL,
@@ -62,54 +61,6 @@ CREATE TABLE asocijacija_pojam(
         ON DELETE CASCADE    
 );
 CREATE INDEX idx_pojam_asocijacija_pojam ON asocijacija_pojam(pojam_id);
-=======
-create table pojam(
-    id serial primary key,
-    kreator_id int not null,
-    sadrzaj text not null unique,
-    constraint fk_kreator_pojam
-        foreign key (kreator_id)
-        references korisnik(id) 
-        on delete restrict
-);
-create index idx_kreator_pojam on pojam(kreator_id);
-
--- imutable. Svaki update kreira novu asocijaciju.
-create table asocijacija(
-    id serial primary key,
-    resenje_id int not null,
-    kreator_id int not null,
-    -- TODO kreirati triger koji popunjava pojam prilikom inserta
-    pojmovi_ids text not null unique, -- Prvi id je id resenja. Nakon toga ide zapeta, pa sortirana lista id pojmova odvojena zapetama, 
-                                    -- cilj je da se osigura jedinstvenost asocijacije. 
-    constraint fk_kreator_asocijacija
-        foreign key (kreator_id)
-        references korisnik(id) 
-        on delete restrict,        
-    constraint fk_resenje_asocijacija
-        foreign key (resenje_id)
-        references pojam(id) 
-        on delete restrict
-);
-create index idx_resenje_asocijacija on asocijacija(resenje_id);
-create index idx_kreator_asocijacija on asocijacija(kreator_id);
-
-create table asocijacija_pojam(
-    asocijacija_id int not null,
-    pojam_id int not null,
-    primary key (asocijacija_id, pojam_id),
-    constraint fk_asocijacija_asocijacija_pojam
-        foreign key (asocijacija_id)
-        references asocijacija(id) 
-        on delete cascade,
-    constraint fk_pojam_asocijacija_pojam
-        foreign key (pojam_id)
-        references pojam(id) 
-        on delete cascade    
-);
-create index idx_pojam_asocijacija_pojam on asocijacija_pojam(pojam_id);
-
->>>>>>> 1d12c57e31db0081f5cd1ce20e99ffbff0427999
 
 
 CREATE TABLE kategorija(
@@ -126,7 +77,6 @@ CREATE TABLE kategorija(
 CREATE INDEX idx_roditelj_kategorija ON kategorija(roditelj_id);
 
 -- igra sadrzi niz asocijacija
-<<<<<<< HEAD
 CREATE TABLE igra(
     id serial PRIMARY KEY,
     kategorija_id INT NOT NULL,
@@ -213,73 +163,3 @@ CREATE OR REPLACE FUNCTION sve_u_mala_slova() -- azurira zadnji unos u tabeli ne
 CREATE TRIGGER pretvori_u_mala -- dodajemo triger prilikom svakog unosa u tabelu neprimenjene_reci.
         AFTER INSERT ON neprimenjene_reci
         EXECUTE PROCEDURE sve_u_mala_slova();
-=======
-create table igra(
-    id serial primary key,
-    kategorija_id int not null,
-    kreator_id int not null,
-    vreme_kreiranja timestamptz not null,
-    naziv varchar(100) not null,
-    opis text,
-    aktivna boolean not null default false,
-    meta json not null,
-    broj_igranja int not null default 0,
-    constraint fk_kategorija_igra
-        foreign key (kategorija_id)
-        references kategorija(id) 
-        on delete restrict,
-    constraint fk_kreator_igra
-        foreign key (kreator_id)
-        references korisnik(id) 
-        on delete restrict
-);
-create index idx_kategorija_igra on igra(kategorija_id, aktivna);
-create index idx_kreator_igra on igra(kreator_id, aktivna);
-create index idx_broj_igranja_igra on igra(broj_igranja, aktivna);
-
-
-create table igra_asocijacija(
-    igra_id int not null,
-    asocijacija_id int not null,
-    primary key(igra_id, asocijacija_id),
-    constraint fk_igra_igra_asocijacija
-        foreign key (igra_id)
-        references igra(id) 
-        on delete cascade,
-    constraint fk_asocijacija_igra_asocijacija
-        foreign key (asocijacija_id)
-        references asocijacija(id) 
-        on delete cascade
-);
-create index idx_asocijacija_igra_asocijacija on igra_asocijacija(asocijacija_id);
-
-create table resena_igra(
-    igra_id int not null,
-    korisnik_id int not null,
-    primary key(igra_id, korisnik_id),
-    constraint fk_igra_resena_igra
-        foreign key (igra_id)
-        references igra(id) 
-        on delete cascade,
-    constraint fk_korisnik_resena_igra
-        foreign key (korisnik_id)
-        references korisnik(id) 
-        on delete restrict
-);
-create index idx_korisnik_resena_igra on resena_igra(korisnik_id);
-
-create table resena_asocijacija(
-    asocijacija_id int not null,
-    korisnik_id int not null,
-    primary key (asocijacija_id, korisnik_id),
-    constraint fk_asocijacija_resena_asocijacija
-        foreign key (asocijacija_id)
-        references asocijacija(id) 
-        on delete cascade,
-    constraint fk_korisnik_resena_asocijacija
-        foreign key (korisnik_id)
-        references korisnik(id) 
-        on delete restrict
-);
-create index idx_korisnik_resena_asocijacija on resena_asocijacija(asocijacija_id);
->>>>>>> 1d12c57e31db0081f5cd1ce20e99ffbff0427999
